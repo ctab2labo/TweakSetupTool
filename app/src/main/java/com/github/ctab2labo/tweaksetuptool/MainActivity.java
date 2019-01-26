c141335495426ae725da766418e57dc51a030add
shiosefine <shiosefine@gmail.com>
2019-01-26 21:55:25 +0900

    commit

--- app/src/main/java/com/github/ctab2labo/tweaksetuptool/MainActivity.java
+++ app/src/main/java/com/github/ctab2labo/tweaksetuptool/MainActivity.java
@@ -6,12 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.ctab2labo.tweaksetuptool.task.FileDownloadTask;
+
+import java.io.File;
 
 public class MainActivity extends Activity implements View.OnClickListener{
     private TextView titleView;
     private ProgressBar progress;
     private TextView progressView;
     private Button button;
+
+    private int progressMax = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +31,36 @@ public class MainActivity extends Activity implements View.OnClickListener{
         button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(this);
+        progress.setMax(100);
     }
 
     @Override
     public void onClick(View view) {
+        button.setEnabled(false);
+        titleView.setText(getString(R.string.text_download_list));
+        setParcent(0);
+        setProgressMax(1);
+        FileDownloadTask task = new FileDownloadTask(getString(R.string.url_list), new File(getFilesDir(), "deliveryList.json"));
+        task.setSuccessListner(urlListSuccessListner);
+        task.execute();
+    }
 
+    private final FileDownloadTask.OnSuccessListner urlListSuccessListner = new FileDownloadTask.OnSuccessListner() {
+        @Override
+        public void onSuccess(boolean bool) {
+            setParcent(1);
+
+        }
+    };
+
+    private void setProgressMax(int max) {
+        progressMax = max;
+    }
+
+    private void setParcent(int progress) {
+        progress = progress * 100 / progressMax;
+        if (progress > 100) progress = 100;
+        this.progress.setProgress(progress);
+        this.progressView.setText(new StringBuilder().append(progress).append("%"));
     }
 }
--- app/src/main/java/com/github/ctab2labo/tweaksetuptool/task/FileDownloadTask.java
+++ app/src/main/java/com/github/ctab2labo/tweaksetuptool/task/FileDownloadTask.java
@@ -1,10 +1,117 @@
 package com.github.ctab2labo.tweaksetuptool.task;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.util.Log;
 
-public class FileDownloadTask extends AsyncTask<String, Void, Boolean> {
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class FileDownloadTask extends AsyncTask<Void, Integer, Boolean> {
+    private final String TAG = "TweakSetupTool";
+    private final int BUFFER_SIZE = 1024;
+    private String urlString;
+    private File file;
+    private OnProgressUpdateListner updateListner;
+    private OnSuccessListner successListner;
+    private int totalByte;
+    private int previosInt;
+    private byte[] buffer = new byte[BUFFER_SIZE];
+
+    public FileDownloadTask(String url, File file) {
+        urlString = url;
+        this.file = file;
+    }
+
     @Override
-    protected Boolean doInBackground(String... string) {
-        return null;
+    protected Boolean doInBackground(Void... obj) {
+        // URLをもとに初期化
+        URLConnection connection;
+        FileOutputStream fileOutputStream;
+        InputStream inputStream;
+        try {
+            URL url = new URL(urlString);
+            connection = url.openConnection();
+            inputStream = connection.getInputStream();
+            fileOutputStream = new FileOutputStream(file);
+        } catch (Exception e) {
+            Log.e(TAG, "FileDownloadTask:Exception", e);
+            return false;
+        }
+        connection.setReadTimeout(5000);
+        connection.setConnectTimeout(30000);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, BUFFER_SIZE);
+        totalByte = connection.getContentLength();
+        int currentByte = 0;
+        previosInt = 0;
+
+        // 読み取れたデータだけ書き込む。
+        try {
+            int len;
+            while((len = bufferedInputStream.read(buffer)) != -1){
+                fileOutputStream.write(buffer, 0, len);
+                currentByte += len;
+                updateProgress(currentByte, totalByte);
+            }
+        } catch(IOException e) {
+            Log.d(TAG, "FileDownloadTask:IOException", e);
+            return false;
+        }
+
+        try {
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            bufferedInputStream.close();
+        } catch(IOException e) {
+            Log.e(TAG, "FileDownloadTask:IOException", e);
+            return false;
+        }
+        return true;
+    }
+
+    private void updateProgress(int i, int i2) {
+        // 100分率を計算。変更があれば、パブリッシュ
+        i = i * 100 / i2;
+        i = i > 100 ? 100 : i;
+        if (previosInt != i) {
+            previosInt = i;
+            publishProgress(i);
+        }
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        if (updateListner != null) {
+            updateListner.onUpdate(values[0]);
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        if (successListner != null) {
+            successListner.onSuccess(aBoolean);
+        }
+    }
+
+    public interface OnProgressUpdateListner {
+        void onUpdate(int i);
+    }
+
+    public interface OnSuccessListner {
+        void onSuccess(boolean bool);
+    }
+
+    public void setSuccessListner(OnSuccessListner successListner) {
+        this.successListner = successListner;
+    }
+
+    public void setUpdateListner(OnProgressUpdateListner updateListner) {
+        this.updateListner = updateListner;
     }
 }
