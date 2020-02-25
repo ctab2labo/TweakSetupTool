910cf1d5f4168e88c39e43f615c3102dce4f5549
shiragifine <shiragifine@outlook.jp>
2020-02-25 16:51:56 +0900

    Fixed

--- app/src/main/java/com/github/ctab2labo/tweaksetuptool/app_downloader/json/AppInfo.java
+++ app/src/main/java/com/github/ctab2labo/tweaksetuptool/app_downloader/json/AppInfo.java
@@ -13,8 +13,6 @@ import com.github.ctab2labo.tweaksetuptool.app_downloader.task.InstallBaseTask;
 import java.io.File;
 
 public class AppInfo implements Parcelable {
-    private static final long serialVersionUID = 1L;
-
     public static final int PROGRESS_NONE = -1;
     public static final int PROGRESS_INSTALLING = -2;
     public static final int PROGRESS_DOWNLOAD_FAILED = -3;
